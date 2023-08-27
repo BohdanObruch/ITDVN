5e7d62f2c8324264fad5cf2eaa8f85aa01778001
BohdanObruch <bodan1992@gmail.com>
2023-08-27 14:06:22 +0300

    fixed the code

--- itdvn_project_tests/helpers/helper.py
+++ itdvn_project_tests/helpers/helper.py
@@ -2,7 +2,6 @@ from selene import browser, be, command
 
 
 def close_advertising():
-    # Check for cookies popup
     browser.with_(timeout=10).element('.promocode').should(be.visible).perform(command.js.remove)
     browser.with_(timeout=10).element('.cookies-message').should(be.visible).perform(command.js.remove)
     browser.with_(timeout=10).element('#share').should(be.visible).perform(command.js.remove)
